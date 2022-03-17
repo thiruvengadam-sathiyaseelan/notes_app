fb3e61bd0338f443bb25b804fbf0a1e7baac104d
Thiruvengadam Sathiyaseelan <thiruvengadam.sathiyaseelan@freshworks.com>
2022-03-17 16:48:22 +0530

    code cleanup

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,5 @@
 class Note < ActiveRecord::Base
-  belongs_to :folder, dependent: :delete
+  belongs_to :folder
   validates :name, :presence => true, :length => { :minimum => 2, :maximum => 50 }
   validates :content, :presence => true, :length => { :maximum => 500 }
 end
